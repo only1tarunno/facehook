33c11b41ab3237053792468cb68d2acf167274bd
only1tarunno <tarunno281@gmail.com>
2024-03-02 17:52:01 +0600

    aa

--- src/components/Posts/PostComments.jsx
+++ src/components/Posts/PostComments.jsx
@@ -1,16 +1,33 @@
 /* eslint-disable react/prop-types */
-import { useAvatar } from "../../hooks/useAvatar";
+import { useState } from "react";
+
 import PostCommentList from "./PostCommentList";
+import useAxios from "../../hooks/useAxios";
+import useAuth from "../../hooks/useAuth";
 
 const PostComments = ({ post }) => {
-  const { avatarURL } = useAvatar(post);
+  const { api } = useAxios();
+  const { auth } = useAuth();
+
+  const [comments, setComments] = useState(post?.comments);
+  const [comment, setComment] = useState("");
+
+  const addComment = async (e) => {
+    const keyCode = e.keyCode;
+    if (keyCode === 13) {
+      const res = await api.patch(`/posts/${post?.id}/comment`, { comment });
+      if (res.status === 200) {
+        setComments([...res.data.comments]);
+      }
+    }
+  };
 
   return (
     <div>
       <div className="flex-center mb-3 gap-2 lg:gap-4">
         <img
           className="max-w-7 max-h-7 rounded-full lg:max-h-[34px] lg:max-w-[34px]"
-          src={avatarURL}
+          src={`${import.meta.env.VITE_SERVER_BASE_URL}/${auth?.user?.avatar}`}
           alt="avatar"
         />
 
@@ -19,6 +36,9 @@ const PostComments = ({ post }) => {
             type="text"
             className="h-8 w-full rounded-full bg-lighterDark px-4 text-xs focus:outline-none sm:h-[38px]"
             name="post"
+            value={comment}
+            onChange={(e) => setComment(e.target.value)}
+            onKeyDown={(e) => addComment(e)}
             id="post"
             placeholder="What's on your mind?"
           />
@@ -28,7 +48,7 @@ const PostComments = ({ post }) => {
         <button className="text-gray-300 max-md:text-sm">All Comment ▾</button>
       </div>
 
-      <PostCommentList comments={post?.comments} />
+      <PostCommentList comments={comments} />
     </div>
   );
 };
