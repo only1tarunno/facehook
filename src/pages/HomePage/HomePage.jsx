35ca625cae484b4679732c43d196193cf4081c7e
only1tarunno <tarunno281@gmail.com>
2024-03-02 14:37:19 +0600

    k

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,5 +1,39 @@
+import { useEffect, useReducer } from "react";
+import { initialState, postReducer } from "../../reducers/PostReducer";
+import useAxios from "../../hooks/useAxios";
+import PostList from "../../components/Posts/PostList";
+import { actions } from "../../actions";
+
 const HomePage = () => {
-  return <>fff</>;
+  const [state, dispatch] = useReducer(postReducer, initialState);
+  const { api } = useAxios();
+
+  useEffect(() => {
+    dispatch({ type: actions.post.DATA_FETCHING });
+
+    const fetchpost = async () => {
+      try {
+        const res = await api.get(`/posts`);
+        if (res?.status) {
+          dispatch({ type: actions.post.DATA_FETCHED, data: res.data });
+        }
+      } catch (error) {
+        dispatch({ type: actions.post.DATA_FETCH_ERROR, error: error.message });
+      }
+    };
+
+    fetchpost();
+  }, []);
+
+  if (state?.loading) {
+    return <div>loading...</div>;
+  }
+
+  return (
+    <>
+      <PostList posts={state?.posts} />
+    </>
+  );
 };
 
 export default HomePage;
